8fbf56e2d47372819ab878f0c05c1bfabc5b71ce
Others <Others@india-PC>
2017-04-09 19:46:14 +0530

    Added CommandLineRunner.

--- SpringBootBasicWebApp/src/main/java/com/abhijeet/springboot/SpringBootBasicWebApp.java
+++ SpringBootBasicWebApp/src/main/java/com/abhijeet/springboot/SpringBootBasicWebApp.java
@@ -1,11 +1,33 @@
 package com.abhijeet.springboot;
 
 import org.springframework.boot.SpringApplication;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootBasicWebApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBasicWebApp.class, args);
+	}
+
+	/**
+	 * Runs on start up. It retrieves all the beans that were created either by your app or were automatically added
+	 */
+	@Bean
+	public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 }
