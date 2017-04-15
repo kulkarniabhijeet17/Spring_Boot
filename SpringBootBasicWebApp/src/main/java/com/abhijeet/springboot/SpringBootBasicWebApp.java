a72d324e48bc7e443e328b10e081cdec8ba03bc6
Others <Others@india-PC>
2017-04-15 15:30:16 +0530

    Alternative way for CommandLineRunner.

--- SpringBootBasicWebApp/src/main/java/com/abhijeet/springboot/SpringBootBasicWebApp.java
+++ SpringBootBasicWebApp/src/main/java/com/abhijeet/springboot/SpringBootBasicWebApp.java
@@ -12,13 +12,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootBasicWebApp {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBasicWebApp.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringBootBasicWebApp.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 	/**
 	 * Runs on start up. It retrieves all the beans that were created either by your app or were automatically added
 	 */
-	@Bean
+	/*@Bean
 	public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
 		return args -> {
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
@@ -29,5 +37,5 @@ public class SpringBootBasicWebApp {
 				System.out.println(beanName);
 			}
 		};
-	}
+	}*/
 }
