277587b1cd363b82c10ec8cbd1b9106e86b4dc4d
Anvi <Anvi@Admin-PC>
2020-03-14 12:18:13 +0530

    Spring Boot HTML View Resolver from Java Configuration.

---
+++ SpringBootHTMLViewResolverJavaConfig/src/main/java/com/springboot/config/HTMLViewConfiguration.java
@@ -0,0 +1,26 @@
+package com.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class HTMLViewConfiguration extends WebMvcConfigurerAdapter {
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/html/");
+		resolver.setSuffix(".html");
+		return resolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
