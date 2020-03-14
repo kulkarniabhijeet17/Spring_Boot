1994ba5ed078499dfa6e18ebf14d27414076081a
Anvi <Anvi@Admin-PC>
2020-03-14 11:43:22 +0530

    Spring Boot JSP View Resolver Example with Java Based Configuration.

---
+++ SpringBootJSPViewResolverJavaConfig/src/main/java/com/springboot/config/JSPViewConfiguration.java
@@ -0,0 +1,28 @@
+package com.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class JSPViewConfiguration extends WebMvcConfigurerAdapter {
+	/*@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}*/
+
+	@Bean
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
