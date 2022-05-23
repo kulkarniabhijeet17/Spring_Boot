fb5d3120079d6beb4ede56065de50dc3f67b03d9
Abhijeet Ramesh Kulkarni <kulkarniabhijeet17@gmail.com>
2022-05-23 17:35:55 +0530

    Internationalization and Localization of RESTful Web Services.

---
+++ InternationalizationAndLocalization/src/main/java/com/springboot/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.springboot.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class UserController {
+    @Autowired
+    private MessageSource messageSource;
+
+    // Internationalization
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
+}
